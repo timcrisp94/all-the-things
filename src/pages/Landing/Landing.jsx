fe37ea37d1453418aabd1c38ada96ab580a188d6
Tim Crisp <timothycrisp@gmail.com>
2022-03-24 10:45:43 -0500

    tim is ben now

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -5,7 +5,7 @@ const Landing = () => {
     <>
       {/* All the <Link> components should live here */}
       <h1>All-The-Things</h1>
-      <Link to="/the-manliest-things">Ben's Things</Link><br/>
+      <Link to="/the-manliest-things">Tim's Things</Link><br/>
       <Link to="/the-functional-things">Evan's Things</Link><br/>
       <Link to="/the-well-styled-things">David's Things</Link><br/>
       <Link to="/the-silly-things">Hunter's Things</Link><br/>
--- src/pages/ManliestThings/ManliestThings.jsx
+++ src/pages/ManliestThings/ManliestThings.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const ManliestThings = (props) => {
   return (
     <>
-      <h1>Ben's Things</h1>
+      <h1>Tim's Things</h1>
       <Link to="/">Home</Link>
 
       {props.things.map((thing, idx) => 
